144f70612c677add1039a309ddabe3927f0915d5
WuHaijia <2319918370@qq.com>
2026-03-09 10:59:23 +0800

    feat 新增todolist中间件、summarization中间件等，新增ask_clarification工具，但其对应的中间件尚未添加，完善了测试脚本

---
+++ backend/src/config/app_config.py
@@ -0,0 +1,60 @@
+from typing import Self
+from src.config.model_config import ModelConfig
+from src.config.tool_config import ToolConfig, ToolGroupConfig
+from dotenv import load_dotenv
+load_dotenv()
+from pathlib import Path
+import yaml
+from pydantic import BaseModel, Field, ConfigDict
+
+
+class AppConfig(BaseModel):
+    """Config for the DeerFlow application"""
+
+    models: list[ModelConfig] = Field(default_factory=list, description="Available models")
+    # TODO: 不太懂沙盒，暂时跳过
+    #sandbox: SandboxConfig = Field(description="Sandbox configuration")
+    tools: list[ToolConfig] = Field(default_factory=list, description="Available tools")
+    tool_groups: list[ToolGroupConfig] = Field(default_factory=list, description="Available tool groups")
+    # TODO: 不太懂技能，暂时跳过
+    #skills: SkillsConfig = Field(default_factory=SkillsConfig, description="Skills configuration")
+    #extensions: ExtensionsConfig = Field(default_factory=ExtensionsConfig, description="Extensions configuration (MCP servers and skills state)")
+    model_config = ConfigDict(extra="allow", frozen=False)
+
+    def from_file(cls, config_path: str|None=None)->Self:
+        """
+        从文件加载应用配置。
+
+        参数：
+            config_path (str): 配置文件路径。
+        返回：
+            config (AppConfig): 应用配置。
+        """
+        path=Path(config_path)
+        if not path.exists():
+            raise FileNotFoundError(f"Config file not found: {config_path}")
+        with open(path,  encoding="utf-8") as f:
+            config_data=yaml.safe_load(f)
+        # 处理 None 值，确保所有列表字段都有默认值
+        if config_data is None:
+            config_data = {}
+        if config_data.get("tools") is None:
+            config_data["tools"] = []
+        if config_data.get("models") is None:
+            config_data["models"] = []
+        if config_data.get("tool_groups") is None:
+            config_data["tool_groups"] = []
+        reseult=cls.model_validate(config_data) 
+        return reseult
+
+
+def get_app_config() -> AppConfig:
+    """
+    获取应用配置。
+
+    返回：
+        config (AppConfig): 应用配置。
+    """
+    #TODO: 配置文件路径暂时使用硬编码
+    _app_config = AppConfig().from_file("config.yaml")
+    return _app_config
---
+++ backend/src/tools/tools.py
@@ -0,0 +1,40 @@
+from langchain.tools import BaseTool
+from src.config.app_config import get_app_config
+from src.reflection.resolvers import resolve_variable
+from src.tools.builtiins import ask_clarification_tool
+import logging
+logger = logging.getLogger(__name__)#创建一个与当前模块同名的日志记录器实例 ，用于在该模块中记录各种级别的日志信息。
+
+# 内建工具
+BUILTIN_TOOLS = [
+    #present_file_tool,TODO：这个，也许暂时不需要……
+    ask_clarification_tool,
+]
+# 子代理工具
+SUBAGENT_TOOLS = [
+    #task_tool,
+]
+def get_available_tools(
+    groups: list[str] | None = None,
+    include_mcp: bool = True,
+    subagent_enabled: bool = True,
+)->list[BaseTool]:
+    """
+    返回可用的工具列表。
+
+    参数：
+        groups (list[str] | None): 工具组列表。
+        include_mcp (bool): 是否包含MCP工具。
+        subagent_enabled (bool): 是否启用子智能体。
+    返回：
+        tools (list[Tool]): 可用的工具列表。
+    """
+    config=get_app_config()
+    loaded_tools=[resolve_variable(tool.use,BaseTool) for tool in config.tools if groups is None or tool.group in groups]
+    # TODO: 添加MCP工具
+    builtin_tools=BUILTIN_TOOLS.copy()
+    if subagent_enabled:
+        builtin_tools.extend(SUBAGENT_TOOLS)
+        logger.info(f"Enabled subagent tools: {SUBAGENT_TOOLS}")
+
+    return loaded_tools+builtin_tools

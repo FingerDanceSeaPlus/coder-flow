18b745c32aabd298d4639956983350fa2a57621e
WuHaijia <12843439+Wu_Hai_Jia@user.noreply.gitee.com>
2026-03-16 16:59:21 +0800

    feat 实现了前后端交互

---
+++ backend/app/gateway/routers/__init__.py
@@ -0,0 +1,3 @@
+from . import artifacts, mcp, models,  suggestions
+__all__ = ["artifacts", "mcp", "models",  "suggestions"]
+#__all__ = ["artifacts", "mcp", "models", "skills", "suggestions", "uploads"]
---
+++ backend/src/agents/lead_agent/__init__.py
@@ -0,0 +1,4 @@
+from src.agents.lead_agent.agent import make_lead_agent
+__all__ = [
+    "make_lead_agent",
+]
